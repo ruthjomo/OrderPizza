9bc9c2aa618c6516a60bdfe44440b6adc2ae5dfa
ruthjomo <ruthjomo19@gmail.com>
2020-08-03 18:54:02 +0300

    Added codes to file

--- script.js
+++ script.js
@@ -14,4 +14,17 @@ $(document).ready(function(){
     $(".contact-us").click(function(){
         $(".footer").show();
     })
+});
+var add=function(pricevalue1,pricevalue2){
+    return(pricevalue1 + pricevalue1)   
+}
+$(document).ready(function(){
+    $("#add-to-cart").click(function(event){
+        event.preventDefault();
+        var pricevalue1 = parseInt($("#crust").val());
+        var pricevalue2 = parseInt($("#toppings").val());
+        var result = add(pricevalue1 + pricevalue2); 
+        $("#output").text(result);
+    
+    })
 })
